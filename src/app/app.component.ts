bd5d35896fef254ec6b439e0c6381324ff8d47ae
Marty Mazurik <marty@martymazurik.com>
2024-08-07 12:43:51 -0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSnackBar,
+         MatSnackBarAction,
+         MatSnackBarActions,
+         MatSnackBarLabel,
+         MatSnackBarRef,
+         } from '@angular/material/snack-bar';
+import  flagsmith  from 'flagsmith'
 
 @Component({
   selector: 'app-root',
@@ -10,4 +17,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mtb-announcement';
+  loading = true;
+  environmentID = 'bszYmQJuVQkXN9X4Mckv7Q';     // Flagsmith Client-side AppId/Environment ID
+  durationInSeconds = 60;
+
+  constructor(private _snackBar: MatSnackBar) {
+
+    const { environmentID, handleFlags, handleFlagsError } =this;
+
+    flagsmith.init({
+      environmentID,
+      onChange: handleFlags,
+      onError: handleFlagsError
+    });
+
+  }
+
+  handleFlags = () => {
+
+    this.loading = false;
+
+    let announcement = flagsmith.getValue('mtb-announcement') as string;
+
+    if( announcement !== "" ) {
+        this._snackBar.open(announcement, "Dismiss", {
+          duration: this.durationInSeconds * 1000,
+        });
+      }
+  };
+
+  handleFlagsError = () => {
+
+  };
+
 }
